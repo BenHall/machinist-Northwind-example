2c3e2d371003971ba8b61e9b1d32bd571414d850
BenHall <ben@benhall.me.uk>
2010-04-27 23:58:56 +0100

    Added faker

--- create_data.rb
+++ create_data.rb
@@ -1,7 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + "/blueprints")
+require 'faker'
 require 'dir_extension'
 Dir.require_all 'northwind'
 
 Sham.reset
 Sham.name { (1..10).map { ('a'..'z').to_a.rand }.join }
 puts Sham.name
+
+Sham.define do
+  title { Faker::Lorem.words(5).join(' ') }
+  name  { Faker::Name.name }
+  body  { Faker::Lorem.paragraphs(3).join("\n\n") }
+end
+
+puts Sham.title
